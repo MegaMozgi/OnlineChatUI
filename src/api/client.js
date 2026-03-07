7260ba94b224ad97217fefc495f22d8531cdbcc5
tk-dev <124786891+MegaMozgi@users.noreply.github.com>
2026-03-07 13:24:26 +0200

    redirection to login if token expired

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,9 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import { BASE_URL } from "./endpoints";
 
 let AUTH_TOKEN = "AuthToken";
+var navigate = useNavigate();
 
 export const apiClient = axios.create({
     baseURL: BASE_URL,
@@ -39,7 +41,7 @@ apiClient.interceptors.response.use(
     response => response,
     async (error) => {
         if (error.response.status === 401) {
-            console.log(error.toJSON());
+            navigate("/login");
         }
         else {
             console.log(error.toJSON());
